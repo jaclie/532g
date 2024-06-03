ef2d00eb2fa34c0738b4ef2b85f74905beacce02
jaclie <129698049+jaclie@users.noreply.github.com>
2024-06-04 09:49:07 +1200

    ranking

---
+++ src/pages/DetialPages.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+
+import styles from './index.module.scss';
+
+const DetialPages = () => {
+    return (
+        <div className={styles.rootRadar}>
+            <div className={styles.group68}>
+                <p className={styles.block4441}>
+                    <span className={styles.block4441}>block: </span>
+                    <span className={styles.block44411}>44</span>
+                    <span className={styles.block44412}> : </span>
+                    <span className={styles.block44413}>41</span>
+                </p>
+                <div className={styles.autoWrapper}>
+                    <p className={styles.score11090}>
+                        <span className={styles.score11090}>score: </span>
+                        <span className={styles.score110901}>110 : </span>
+                        <span className={styles.score110902}>90</span>
+                    </p>
+                    <div className={styles.autoWrapper}>
+                        <img
+                            src="https://p26-semi-sign.byteimg.com/tos-cn-i-acvclvrq33/1026d58ebbea40e8a84e3a94b5491f70.svg?rk3s=521bdb00&x-expires=1717459568&x-signature=lNd%2Bl0h12MkysyOmLHYfyOnc%2Fbk%3D"
+                            className={styles.union}
+                        />
+                        <p className={styles.threePointShot1911}>
+                            <span className={styles.threePointShot1911}>three point shot: </span>
+                            <span className={styles.threePointShot19111}>19</span>
+                            <span className={styles.threePointShot19112}> : </span>
+                            <span className={styles.threePointShot19113}>11</span>
+                        </p>
+                        <p className={styles.fieldGoalPercentage4}>
+              <span className={styles.fieldGoalPercentage4}>
+                field goal percentage:{" "}
+              </span>
+                            <span className={styles.fieldGoalPercentage41}>41.30%</span>
+                            <span className={styles.fieldGoalPercentage42}> : </span>
+                            <span className={styles.fieldGoalPercentage43}>42.50%</span>
+                        </p>
+                        <p className={styles.steal138}>
+                            <span className={styles.steal138}>steal: </span>
+                            <span className={styles.steal1381}>13 </span>
+                            <span className={styles.steal1382}>: </span>
+                            <span className={styles.steal1383}>8</span>
+                        </p>
+                        <p className={styles.assist2727}>
+                            <span className={styles.assist2727}>Assist: </span>
+                            <span className={styles.assist27271}>27</span>
+                            <span className={styles.assist27272}> : </span>
+                            <span className={styles.assist27273}>27</span>
+                        </p>
+                    </div>
+                    <p className={styles.freeThrowPercentage1}>
+            <span className={styles.freeThrowPercentage1}>
+              Free throw percentage:{" "}
+            </span>
+                        <span className={styles.freeThrowPercentage11}>100%</span>
+                        <span className={styles.freeThrowPercentage12}> : </span>
+                        <span className={styles.freeThrowPercentage13}>91.67%</span>
+                    </p>
+                </div>
+                <p className={styles.foul2016}>
+                    <span className={styles.foul2016}>foul: </span>
+                    <span className={styles.foul20161}>20</span>
+                    <span className={styles.foul20162}> : </span>
+                    <span className={styles.foul20163}>16</span>
+                </p>
+            </div>
+            <img
+                src="https://p26-semi-sign.byteimg.com/tos-cn-i-acvclvrq33/8d56db1833ab4acb853f76955c2f0daf.svg?rk3s=521bdb00&x-expires=1717459568&x-signature=MTaveY3iZlwVpDjhCLEmbzaJd7k%3D"
+                className={styles.vector72}
+            />
+            <img
+                src="https://p6-semi-sign.byteimg.com/tos-cn-i-acvclvrq33/1fcf0acd5e2c44a69c949b76fd5ee621.svg?rk3s=521bdb00&x-expires=1717459568&x-signature=IRQApk8oIC2n%2FTb5u1PahEcFSVk%3D"
+                className={styles.vector107}
+            />
+            <div className={styles.group20}>
+                <img
+                    src="https://p6-semi-sign.byteimg.com/tos-cn-i-acvclvrq33/df05e990396a4ea1b9b89bf7d8ef261a.svg?rk3s=521bdb00&x-expires=1717459568&x-signature=NPG%2BuflXyShzEKymDreT8bLQ6r8%3D"
+                    className={styles.vector}
+                />
+                <p className={styles.warriors}>Warriors</p>
+            </div>
+            <div className={styles.group69}>
+                <img
+                    src="https://p26-semi-sign.byteimg.com/tos-cn-i-acvclvrq33/8875eabb0cbb4b15b24e10fc721883b4.svg?rk3s=521bdb00&x-expires=1717459568&x-signature=Lm1U4nSd03Wf%2FLofDYn%2F8IDnRfM%3D"
+                    className={styles.vector}
+                />
+                <p className={styles.celtics}>Celtics</p>
+            </div>
+            <p className={styles.nBAFinalsGame6On2021}>
+        <span className={styles.nBAFinalsGame6On2021}>
+          NBA Finals&nbsp;&nbsp;game 6 on 2021-2022
+        </span>
+                <span className={styles.nBAFinalsGame6On20211}>
+          (day6.17)
+          <br />
+        </span>
+                <span className={styles.nBAFinalsGame6On20212}>Warriors vs. Celtics</span>
+            </p>
+        </div>
+    );
+}
+
+export default DetialPages;
